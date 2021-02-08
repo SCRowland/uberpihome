02ae6a384a4facb331a1950f9da801ce619b96d0
Simon Rowland <simon@futrli.com>
2021-02-08 14:20:59 +0000

    Add messages to deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 
 # an ugly / hacky deployment, but good enough for my home RPi setup.
+echo "Copying files into place"
 scp etc/nginx/sites-available/*.conf uberpi:/home/pi
 scp html/index.html uberpi:/home/pi
 
+echo "Restarting nginx"
 ssh uberpi "sudo service nginx restart"
+
+echo "Deployment complete"
